0ac407c11588420771a5a9e506d952e94a3f4edd
sravanthi <sravanthishankara@gmail.com>
2026-06-21 19:16:44 +0530

    added basic changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
@@ -6,12 +6,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, (err) => {
-    if (err) {
-        return console.log('Something bad happened', err);
-    }
-    console.log(`Server is listening on ${port}`);
+  if (err) {
+    return console.log("Something bad happened", err);
+  }
+  console.log(`Server is listening on ${port}`);
 });
-
-
 
 module.exports = app;
